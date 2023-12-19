4f9473520b31c74fd2a9eecec9bae45bafb4c352
Ye0ngJae <71473974+Ye0ngjae@users.noreply.github.com>
2023-12-19 23:27:11 +0900

    [Bronze II] Title: 도비의 영어 공부, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2386. 도비의 영어 공부/도비의 영어 공부.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+ 
+using namespace std;
+ 
+int main() {
+ 
+    char ch;
+    string str;
+ 
+    while(1){
+        cin>>ch;
+        getline(cin, str);
+ 
+        if(ch=='#'){
+            break;
+        }
+ 
+        int len=str.length();
+ 
+        int cnt=0;
+ 
+        for(int i=0; i<len; i++){
+            if(str[i]==ch||str[i]==toupper(ch)){ // keypoint
+                cnt++;
+            }
+        }
+        cout<<ch<<" "<<cnt<<"\n";
+    }
+}
