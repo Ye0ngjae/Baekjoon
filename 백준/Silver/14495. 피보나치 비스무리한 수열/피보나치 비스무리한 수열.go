3d2875c84a55534f11e8c5a33500780f7da97498
Ye0ngjae <op@ye0ngjae.com>
2024-10-10 17:19:43 +0900

    [Silver IV] Title: 피보나치 비스무리한 수열, Time: 4 ms, Memory: 884 KB -BaekjoonHub

---
+++ 백준/Silver/14495. 피보나치 비스무리한 수열/피보나치 비스무리한 수열.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+)
+
+func main() {
+	runtime.GOMAXPROCS(runtime.NumCPU())
+
+	var N int
+	fmt.Scan(&N)
+	fibo := make([]int, 1)
+
+	for i := 1; i <= N; i++ {
+		if i == 1 || i == 2 {
+			fibo = append(fibo, 1)
+			continue
+		}
+		fibo = append(fibo, fibo[i-1]+fibo[i-3])
+	}
+
+	fmt.Println(fibo[N])
+
+}
