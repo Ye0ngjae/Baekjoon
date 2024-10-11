cf56dd3bf8e4a2f88fa0a0601a74c885e898eaca
Ye0ngjae <op@ye0ngjae.com>
2024-10-11 10:10:43 +0900

    [Silver IV] Title: 피보나치 수 7, Time: 112 ms, Memory: 36392 KB, Score: 201 point -BaekjoonHub

--- 백준/Silver/15624. 피보나치 수 7/피보나치 수 7.go
+++ 백준/Silver/15624. 피보나치 수 7/피보나치 수 7.go
@@ -8,18 +8,18 @@ import (
 func main() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
 
-	var N int
+	var N int64
 	fmt.Scan(&N)
-	fibo := make([]int, 1)
+	fibo := make([]int64, 1)
 
-	for i := 1; i <= N; i++ {
+	for i := int64(1); i <= N; i++ {
 		if i == 1 || i == 2 {
 			fibo = append(fibo, 1)
 			continue
 		}
-		fibo = append(fibo, fibo[i-1]+fibo[i-2])
+		fibo = append(fibo, (fibo[i-1]+fibo[i-2])%1000000007)
 	}
 
-	fmt.Println(fibo[N]%1000000007)
+	fmt.Println(fibo[N])
 
 }
